dd1d1b93fc0553e858901237b0c691c3601a7035
Muhammad Dennisa <mdennisa@gmail.com>
2023-10-11 13:16:48 +0800

    set img_url

--- js/data.js
+++ js/data.js
@@ -1,35 +1,36 @@
+const img_url = './images/'
 const data = {
     'project-1': [
         {
-            src: './images/project-1a.jpg'
+            src: `${img_url}project-1a.jpg`
         },
         {
-            src: './images/project-1b.jpg'
+            src: `${img_url}project-1b.jpg`
         },
         {
-            src: './images/project-1c.jpg'
+            src: `${img_url}project-1c.jpg`
         }
     ],
     'project-2': [
         {
-            src: './images/project-2a.jpg'
+            src: `${img_url}project-2a.jpg`
         },
         {
-            src: './images/project-2b.jpg'
+            src: `${img_url}project-2b.jpg`
         },
         {
-            src: './images/project-2c.jpg'
+            src: `${img_url}project-2c.jpg`
         }
     ],
     'project-3': [
         {
-            src: './images/project-3a.jpg'
+            src: `${img_url}project-3a.jpg`
         },
         {
-            src: './images/project-3b.jpg'
+            src: `${img_url}project-3b.jpg`
         },
         {
-            src: '../images/project-3c.jpg'
+            src: `${img_url}project-3c.jpg`
         }
     ],
 }
