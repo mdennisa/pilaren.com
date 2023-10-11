53a0165436bec57155e392e091c271910907caa0
Muhammad Dennisa <mdennisa@gmail.com>
2023-10-11 13:07:54 +0800

    Fix img path

--- js/data.js
+++ js/data.js
@@ -1,32 +1,32 @@
 const data = {
     'project-1': [
         {
-            src: '../images/project-1a.jpg'
+            src: './images/project-1a.jpg'
         },
         {
-            src: '../images/project-1b.jpg'
+            src: './images/project-1b.jpg'
         },
         {
-            src: '../images/project-1c.jpg'
+            src: './images/project-1c.jpg'
         }
     ],
     'project-2': [
         {
-            src: '../images/project-2a.jpg'
+            src: './images/project-2a.jpg'
         },
         {
-            src: '../images/project-2b.jpg'
+            src: './images/project-2b.jpg'
         },
         {
-            src: '../images/project-2c.jpg'
+            src: './images/project-2c.jpg'
         }
     ],
     'project-3': [
         {
-            src: '../images/project-3a.jpg'
+            src: './images/project-3a.jpg'
         },
         {
-            src: '../images/project-3b.jpg'
+            src: './images/project-3b.jpg'
         },
         {
             src: '../images/project-3c.jpg'
