77770383a43f73abd9abc2d6f5c172935f907b66
Muhammad Dennisa <mdennisa@gmail.com>
2024-07-19 21:10:02 +0800

    Add images and update projects

--- js/data.js
+++ js/data.js
@@ -52,6 +52,237 @@ const data = {
             src: `${img_url}biodesel-6.jpg`
         }
     ],
+    'project-pipe': [
+        {
+            src: `${img_url}pipe-1.jpg`
+        },
+        {
+            src: `${img_url}pipe-2.jpg`
+        },
+        {
+            src: `${img_url}pipe-3.jpg`
+        },
+        {
+            src: `${img_url}pipe-4.jpg`
+        },
+        {
+            src: `${img_url}pipe-5.jpg`
+        },
+        {
+            src: `${img_url}pipe-6.jpg`
+        },
+        {
+            src: `${img_url}pipe-7.jpg`
+        },
+        {
+            src: `${img_url}pipe-8.jpg`
+        },
+        {
+            src: `${img_url}pipe-9.jpg`
+        },
+        {
+            src: `${img_url}pipe-10.jpg`
+        },
+        {
+            src: `${img_url}pipe-11.jpg`
+        },
+        {
+            src: `${img_url}pipe-12.jpg`
+        },
+        {
+            src: `${img_url}pipe-13.jpg`
+        },
+        {
+            src: `${img_url}pipe-14.jpg`
+        }
+    ],
+    'project-seaport': [
+        {
+            src: `${img_url}seaport-1.jpg`
+        },
+        {
+            src: `${img_url}seaport-2.jpg`
+        },
+        {
+            src: `${img_url}seaport-3.jpg`
+        },
+        {
+            src: `${img_url}seaport-4.jpg`
+        },
+        {
+            src: `${img_url}seaport-5.jpg`
+        },
+        {
+            src: `${img_url}seaport-6.jpg`
+        },
+        {
+            src: `${img_url}seaport-7.jpg`
+        },
+        {
+            src: `${img_url}seaport-8.jpg`
+        },
+        {
+            src: `${img_url}seaport-9.jpg`
+        },
+        {
+            src: `${img_url}seaport-10.jpg`
+        },
+        {
+            src: `${img_url}seaport-11.jpg`
+        },
+        {
+            src: `${img_url}seaport-12.jpg`
+        },
+        {
+            src: `${img_url}seaport-13.jpg`
+        },
+        {
+            src: `${img_url}seaport-14.jpg`
+        },
+        {
+            src: `${img_url}seaport-15.jpg`
+        },
+        {
+            src: `${img_url}seaport-16.jpg`
+        },
+        {
+            src: `${img_url}seaport-17.jpg`
+        }
+    ],
+    'project-shipwharf': [
+        {
+            src: `${img_url}shipw-1.jpg`
+        },
+        {
+            src: `${img_url}shipw-2.jpg`
+        },
+        {
+            src: `${img_url}shipw-3.jpg`
+        },
+        {
+            src: `${img_url}shipw-4.jpg`
+        },
+        {
+            src: `${img_url}shipw-5.jpg`
+        },
+        {
+            src: `${img_url}shipw-6.jpg`
+        },
+    ],
+    'project-steel': [
+        {
+            src: `${img_url}steel-1.jpg`
+        },
+        {
+            src: `${img_url}steel-2.jpg`
+        },
+        {
+            src: `${img_url}steel-3.jpg`
+        },
+        {
+            src: `${img_url}steel-4.jpg`
+        },
+        {
+            src: `${img_url}steel-5.jpg`
+        },
+        {
+            src: `${img_url}steel-6.jpg`
+        },
+        {
+            src: `${img_url}steel-7.jpg`
+        },
+        {
+            src: `${img_url}steel-8.jpg`
+        },
+        {
+            src: `${img_url}steel-9.jpg`
+        },
+        {
+            src: `${img_url}steel-10.jpg`
+        },
+    ],
+    'project-refinery-plant': [
+        {
+            src: `${img_url}refinery-plant-1.jpg`
+        },
+        {
+            src: `${img_url}refinery-plant-2.jpg`
+        },
+    ],
+    'project-erection-he': [
+        {
+            src: `${img_url}erection-he-1.jpg`
+        },
+        {
+            src: `${img_url}erection-he-2.jpg`
+        },
+        {
+            src: `${img_url}erection-he-3.jpg`
+        },
+    ],
+    'project-palm-oil': [
+        {
+            src: `${img_url}palm-oil-1.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-2.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-3.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-wtp1.jpg`,
+            caption: "Water Treatment Plant"
+        },
+        {
+            src: `${img_url}palm-oil-wtp2.jpg`,
+            caption: "Water Treatment Plant"
+        },
+        {
+            src: `${img_url}palm-oil-wtp3.jpg`,
+            caption: "Water Treatment Plant"
+        },
+        {
+            src: `${img_url}palm-oil-wtp4.jpg`,
+            caption: "Water Treatment Plant"
+        },
+        {
+            src: `${img_url}palm-oil-swro-1.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-2.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-3.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-4.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-5.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-6.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-7.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-8.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-9.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-10.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-11.jpg`
+        },
+        {
+            src: `${img_url}palm-oil-swro-12.jpg`
+        },
+    ]
 }
 
 export default data
